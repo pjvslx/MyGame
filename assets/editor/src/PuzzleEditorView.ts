ce66d899750f579820847fa9a878d6d5d556d34d
ylu <253255767@qq.com>
2020-05-19 22:12:04 +0800

    一部分编辑器功能

--- assets/editor/src/PuzzleEditorView.ts
+++ assets/editor/src/PuzzleEditorView.ts
@@ -12,6 +12,9 @@ const {ccclass, property} = cc._decorator;
 
 @ccclass
 class PuzzleEditorView extends cc.Component {
+    static SELECTED_MODEL = {
+        BLOCK : 0
+    }
     @property(cc.Node)
     btnCreate: cc.Node = null;
     @property(cc.EditBox)
@@ -24,20 +27,61 @@ class PuzzleEditorView extends cc.Component {
     gridPb: cc.Prefab = null;
     @property(cc.Node)
     mapNode: cc.Node = null;
+    @property(cc.ToggleContainer)
+    toggleContainer: cc.ToggleContainer = null;
+    @property(cc.Node)
+    blockTmp: cc.Node = null;       // 选择模板
     
     gridWidth: number = 100;
     gridHeight: number = 100;
     gridMap:Array<Array<cc.Node>> = [];
+    radioIndex:number = 0;
 
     onLoad(){
-        this.gridWidth = this.gridHeight = parseInt(this.editBoxEdge.string);
+        this.blockTmp.active = false;
+        this.resetGridWidthHeight();
         this.addEvent();
+    }
+
+    resetGridWidthHeight(){
+        this.gridWidth = this.gridHeight = parseInt(this.editBoxEdge.string);
+        this.blockTmp.setContentSize(this.gridWidth,this.gridHeight);
     }
 
     addEvent(){
         this.btnCreate.on('click',()=>{
             this.handleCreateClicked();
         },this);
+
+        this.mapNode.on(cc.Node.EventType.MOUSE_MOVE,( param )=>{
+            // console.log('cc.Node.EventType.MOUSE_MOVE param = ' , param);
+            this.handleMouseMove(param._x,param._y);
+        },this);
+    }
+
+    handleMouseMove(worldX,worldY){
+        if(this.radioIndex != PuzzleEditorView.SELECTED_MODEL.BLOCK){
+            return;
+        }
+        // console.log('worldX = ' + worldX + ' worldY = ' + worldY);
+        let nodeMapPos = this.mapNode.convertToNodeSpaceAR(cc.v2(worldX,worldY));
+        // console.log('nodeMapPos.x = ' + nodeMapPos.x + ' nodeMapPos.y = ' + nodeMapPos.y);
+        let exist = false;
+        for(let row = 0; row < this.gridMap.length; row++){
+            for(let col = 0; col < this.gridMap[row].length; col++){
+                let grid = this.gridMap[row][col];
+                let rect = cc.rect(grid.x - grid.width/2,grid.y - grid.height/2,grid.width,grid.height);
+                if(rect.contains(nodeMapPos)){
+                    this.blockTmp.active = true;
+                    this.blockTmp.position = grid.position;
+                    exist = true;
+                    break;
+                }
+            }
+        }
+        if(!exist){
+            this.blockTmp.active = false;
+        }
     }
 
     checkCreate(){
@@ -53,6 +97,16 @@ class PuzzleEditorView extends cc.Component {
         return true;
     }
 
+    update(){
+
+    }
+
+    handleRadioClicked(toggle){
+        // alert('toggle name = ' + this.toggleContainer.toggleItems.indexOf(toggle));
+        this.radioIndex = this.toggleContainer.toggleItems.indexOf(toggle);
+        this.blockTmp.active = (this.radioIndex == PuzzleEditorView.SELECTED_MODEL.BLOCK);
+    }
+
     handleCreateClicked(){
         if(!this.checkCreate()){
             return;
@@ -62,7 +116,7 @@ class PuzzleEditorView extends cc.Component {
                 this.gridMap[row][col].destroy();
             }
         }
-        this.gridWidth = this.gridHeight = parseInt(this.editBoxEdge.string);
+        this.resetGridWidthHeight();
         this.gridMap = [];
 
         let rows = parseInt(this.editBoxRow.string);
@@ -77,12 +131,15 @@ class PuzzleEditorView extends cc.Component {
             for(let col = 0; col < cols; col++){
                 let grid = cc.instantiate(this.gridPb);
                 grid.parent = this.mapNode;
+                grid.width = this.gridWidth;
+                grid.height = this.gridHeight;
                 grid.x = leftTopPos.x + col * this.gridWidth;
                 grid.y = leftTopPos.y - row * this.gridHeight;
                 this.gridMap[row][col] = grid;
             }
         }
-
+        // this.mapNode.scaleX = this.gridWidth / 100;
+        // this.mapNode.scaleY = this.gridHeight / 100;
         this.mapNode.setContentSize(cols * this.gridWidth, rows * this.gridHeight);
     }
 
